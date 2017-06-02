4ebdc530c3fef3a40d1c60c07b742ca69b490fb5
Matthew Thomas <me@matthewathomas.com>
2017-06-02 15:26:53 -0400

    Added tests for StreamExtensions

---
+++ CommonTests/StreamExtensionsClass.cs
@@ -0,0 +1,72 @@
+﻿namespace CommonTests
+{
+    using System.IO;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Common;
+
+    [TestClass]
+    public class StreamExtensionsClass
+    {
+        [TestClass]
+        public class ReadMethod
+        {
+            [TestMethod]
+            public void ReadsCorrectBytes()
+            {
+                using (var stream = new MemoryStream(new byte[] { 0x01, 0x02, 0x03 }))
+                {
+                    var data = stream.Read(3);
+                    Assert.IsNotNull(data);
+                    Assert.IsTrue(data.SequenceEqual(new byte[] { 0x01, 0x02, 0x03 }));
+                }
+            }
+        }
+
+        [TestClass]
+        public class ReadChunkMethod
+        {
+            [TestMethod]
+            public void ReadsWhatWriteChunkPutIn()
+            {
+                using (var stream = new MemoryStream())
+                {
+                    stream.WriteChunk(new byte[] { 0x01, 0x02, 0x03 });
+                    stream.Position = 0;
+                    var data = stream.ReadChunk();
+                    Assert.IsNotNull(data);
+                    Assert.IsTrue(data.SequenceEqual(new byte[] { 0x01, 0x02, 0x03 }));
+                }
+            }
+        }
+
+        [TestClass]
+        public class WriteMethod
+        {
+            [TestMethod]
+            public void PutsCorrectNumberOfBytesInStream()
+            {
+                using (var stream = new MemoryStream())
+                {
+                    stream.Write(new byte[] { 0x01, 0x02, 0x03 });
+                    Assert.AreEqual(3, stream.Position);
+                }
+            }
+        }
+
+        [TestClass]
+        public class WriteChunkMethod
+        {
+            [TestMethod]
+            public void PutsCorrectBytesInStream()
+            {
+                using (var stream = new MemoryStream())
+                {
+                    stream.WriteChunk(new byte[] { 0x01, 0x02, 0x03 });
+                    var data = stream.ToArray();
+                    Assert.IsTrue(data.SequenceEqual(new byte[] { 0x03, 0, 0, 0, 0x01, 0x02, 0x03 }));
+                }
+            }
+        }
+    }
+}
