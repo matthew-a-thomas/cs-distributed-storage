946b99b37a633072a9c3ef98a81187eeb9a49fd1
Matthew Thomas <me@matthewathomas.com>
2017-06-02 21:14:01 -0400

    Added sample RSA keys to Resources for testing purposes, and added a way to retrieve those keys (through the new RsaKeyProvider class)

---
+++ SecurityTests/RsaKeyProvider.cs
@@ -0,0 +1,46 @@
+﻿namespace SecurityTests
+{
+    using System;
+    using System.Security.Cryptography;
+    using Newtonsoft.Json;
+    using Properties;
+
+    /// <summary>
+    /// Provides access to some built-in RSA keys useful for testing purposes
+    /// </summary>
+    internal class RsaKeyProvider
+    {
+        /// <summary>
+        /// An RSA key
+        /// </summary>
+        internal readonly RSAParameters RsaKey1;
+
+        /// <summary>
+        /// An RSA key
+        /// </summary>
+        internal readonly RSAParameters RsaKey2;
+
+        internal RsaKeyProvider()
+        {
+            var keys = LoadKeys();
+            RsaKey1 = keys[0];
+            RsaKey2 = keys[1];
+        }
+
+        /// <summary>
+        /// Loads RSA keys from <see cref="Resources"/>
+        /// </summary>
+        private static RSAParameters[] LoadKeys()
+        {
+            var json = Resources.Sample_RSA_keys;
+            if (json == null)
+                throw new Exception();
+            var keys = JsonConvert.DeserializeObject<RSAParameters[]>(json);
+            if (keys == null)
+                throw new Exception();
+            if (keys.Length != 2)
+                throw new Exception();
+            return keys;
+        }
+    }
+}
