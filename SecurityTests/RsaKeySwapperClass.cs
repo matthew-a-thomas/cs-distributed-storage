9b82819f771fec8538a6de0b9f36bd58f20547e7
Matthew Thomas <me@matthewathomas.com>
2017-06-03 14:49:48 -0400

    Fleshed out unit tests

--- SecurityTests/RsaKeySwapperClass.cs
+++ SecurityTests/RsaKeySwapperClass.cs
@@ -1,6 +1,8 @@
 ﻿namespace SecurityTests
 {
     using System;
+    using System.IO;
+    using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Security;
     using Utils;
@@ -8,14 +10,49 @@
     [TestClass]
     public class RsaKeySwapperClass
     {
+        private static readonly RsaKeyProvider KeyProvider = new RsaKeyProvider();
+
         [TestClass]
-        public class TrySwapPublicRsaKeysMethod
+        public class SendOurPublicKeyMethod
         {
             [TestMethod]
-            public void SwapsKeys()
+            public void DoesNotThrowAnException()
             {
                 var swapper = new RsaKeySwapper(new NonsecureCryptoRsa(), new NonsecureEntropy());
-                throw new NotImplementedException();
+                using (var stream = new MemoryStream())
+                    swapper.SendOurPublicKey(stream, KeyProvider.RsaKey1);
+            }
+        }
+
+        [TestClass]
+        public class TryGetTheirPublicKeyMethod
+        {
+            [TestMethod]
+            public void ReturnsWhatSendOurPublicKeySent()
+            {
+                var swapper = new RsaKeySwapper(new NonsecureCryptoRsa(), new NonsecureEntropy());
+                using (var stream = new MemoryStream())
+                {
+                    swapper.SendOurPublicKey(stream, KeyProvider.RsaKey1);
+                    stream.Position = 0;
+                    if (!swapper.TryGetTheirPublicKey(stream, TimeSpan.FromSeconds(1), out var theirs))
+                        throw new Exception("Failed to get their public key");
+                    Assert.IsTrue(theirs.ToBytes().SequenceEqual(KeyProvider.RsaKey1.ToBytes()));
+                }
+            }
+
+            [TestMethod]
+            public void TimesOut()
+            {
+                try
+                {
+                    var swapper = new RsaKeySwapper(new NonsecureCryptoRsa(), new NonsecureEntropy());
+                    using (var stream = new MemoryStream())
+                        swapper.TryGetTheirPublicKey(stream, TimeSpan.FromMilliseconds(50), out _);
+                    throw new Exception("Test failed");
+                }
+                catch (TimeoutException)
+                { }
             }
         }
     }
