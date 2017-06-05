0d055c78b962c7b0c2449c8336bfaf4aa81ad3db
Matthew Thomas <me@matthewathomas.com>
2017-06-05 12:54:24 -0400

    Removed ConnectionTests since those were actually integration tests instead of unit tests, and also because the components' functionalities are individually unit tested elsewhere

--- SecurityTests/SecureStreamFactoryClass.cs
+++ SecurityTests/SecureStreamFactoryClass.cs
@@ -2,10 +2,6 @@
 {
     using System;
     using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Common;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Security;
     using Utils;
@@ -15,59 +11,7 @@
     {
         private static readonly RsaKeyProvider KeyProvider = new RsaKeyProvider();
         private static readonly SecureStreamFactory SecureStreamFactory = new SecureStreamFactory(new NonsecureCryptoRsa(), new NonsecureCryptoSymmetric(), new NonsecureEntropy());
-
-        [TestClass]
-        public class ConnectionTests
-        {
-            [TestMethod]
-            public void TryMakingSecureStreamsAndThenSendingSomething()
-            {
-                (var stream1, var stream2) = Pipe.CreateLinkedPair();
-                using (stream1)
-                {
-                    using (stream2)
-                    {
-                        SecureStream
-                            secureStream1 = null,
-                            secureStream2 = null;
-                        Task.WaitAll(
-                            Task.Run(() =>
-                            {
-                                if (!SecureStreamFactory.TryMakeConnection(
-                                    stream1,
-                                    KeyProvider.RsaKey1,
-                                    TimeSpan.FromSeconds(10),
-                                    out var theirs,
-                                    out var secureStream
-                                ))
-                                    throw new Exception($"Couldn't make {nameof(secureStream1)}");
-                                Assert.IsTrue(theirs.ToBytes().SequenceEqual(KeyProvider.RsaKey2.ToBytes()));
-                                secureStream1 = secureStream;
-                            }),
-                            Task.Run(() =>
-                            {
-                                if (!SecureStreamFactory.TryAcceptConnection(
-                                    stream2,
-                                    KeyProvider.RsaKey2,
-                                    TimeSpan.FromSeconds(10),
-                                    out var theirs,
-                                    out var secureStream
-                                ))
-                                    throw new Exception($"Couldn't make {nameof(secureStream2)}");
-                                Assert.IsTrue(theirs.ToBytes().SequenceEqual(KeyProvider.RsaKey1.ToBytes()));
-                                secureStream2 = secureStream;
-                            })
-                        );
-                        secureStream1.SendDatagram(Encoding.ASCII.GetBytes("Hello world"));
-                        if (!secureStream2.TryReceiveDatagram(TimeSpan.FromSeconds(1), out var data))
-                            throw new Exception($"Didn't receive anything on {nameof(secureStream2)}");
-                        var message = Encoding.ASCII.GetString(data);
-                        Assert.AreEqual("Hello world", message);
-                    }
-                }
-            }
-        }
-
+        
         [TestClass]
         public class TryAcceptConnectionMethod
         {
