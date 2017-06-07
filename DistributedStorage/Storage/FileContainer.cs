90b4ddf39e2768dacaea1c1402d75367db2223ba
Matthew Thomas <me@matthewathomas.com>
2017-06-07 15:18:20 -0400

    Finished FileContainer

--- DistributedStorage/Storage/FileContainer.cs
+++ DistributedStorage/Storage/FileContainer.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using Common;
 
     public sealed class FileContainer<TValue> : IContainer<byte[], TValue>
     {
@@ -20,27 +21,61 @@
             _workingDirectory = workingDirectory;
         }
 
+        private static FileInfo GetFileInfoFor(byte[] key) => new FileInfo(key.ToHex());
+
         public IEnumerable<byte[]> GetKeys()
         {
+            // ReSharper disable once LoopCanBeConvertedToQuery
             foreach (var file in _workingDirectory.EnumerateFiles())
             {
-                if (file.Name)
+                if (file.Name.TryToBytes(out var hashBytes))
+                    yield return hashBytes;
             }
         }
 
         public bool TryAdd(byte[] key, TValue value)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var stream = GetFileInfoFor(key).Open(FileMode.CreateNew, FileAccess.Write, FileShare.None))
+                {
+                    var bytes = _serializer(value);
+                    stream.Write(bytes);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool TryGet(byte[] key, out TValue value)
         {
-            throw new NotImplementedException();
+            value = default(TValue);
+            try
+            {
+                using (var stream = GetFileInfoFor(key).Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    if (!stream.TryImmediateRead(out byte[] bytes))
+                        return false;
+                    value = _deserializer(bytes);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool TryRemove(byte[] key)
         {
-            throw new NotImplementedException();
+            var fileInfo = GetFileInfoFor(key);
+            var exists = fileInfo.Exists;
+            if (exists)
+                fileInfo.Delete();
+            return exists;
         }
     }
 }
