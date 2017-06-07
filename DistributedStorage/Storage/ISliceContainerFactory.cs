5be71de6c48cf05a7fcd9378ec4b134e11750054
Matthew Thomas <me@matthewathomas.com>
2017-06-07 15:34:21 -0400

    Created interface for a slice container factory

---
+++ DistributedStorage/Storage/ISliceContainerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace DistributedStorage.Storage
+{
+    using Common;
+    using Encoding;
+
+    public interface ISliceContainerFactory
+    {
+        IContainer<Hash, Slice> CreateSliceContainer(Manifest forManifest);
+    }
+}
