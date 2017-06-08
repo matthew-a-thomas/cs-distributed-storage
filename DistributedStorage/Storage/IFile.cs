09292ddd349454060b0c0a60b8fdfacc92bf1f23
Matthew Thomas <me@matthewathomas.com>
2017-06-08 09:45:02 -0400

    Added IFile, which is an abstraction of creating streams to read and write something

---
+++ DistributedStorage/Storage/IFile.cs
@@ -0,0 +1,10 @@
+﻿namespace DistributedStorage.Storage
+{
+    using System.IO;
+
+    public interface IFile
+    {
+        bool TryOpenRead(out Stream stream);
+        bool TryOpenWrite(out Stream stream);
+    }
+}
