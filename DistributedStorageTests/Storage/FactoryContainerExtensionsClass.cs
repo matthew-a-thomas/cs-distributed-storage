9fb58a675160993308b0ba2074b9229f4ad7fab4
Matthew Thomas <me@matthewathomas.com>
2017-06-08 10:13:00 -0400

    Stub class for factory container extension methods

---
+++ DistributedStorageTests/Storage/FactoryContainerExtensionsClass.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributedStorageTests.Storage
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FactoryContainerExtensionsClass
+    {
+        [TestMethod]
+        public void Implemented() => throw new NotImplementedException();
+    }
+}
