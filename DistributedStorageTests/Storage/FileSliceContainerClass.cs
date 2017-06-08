feefb8c5d40d72d1c907ebc45d80222de260431d
Matthew Thomas <me@matthewathomas.com>
2017-06-08 09:56:16 -0400

    Initial stub for FileSliceContainer class's unit tests

---
+++ DistributedStorageTests/Storage/FileSliceContainerClass.cs
@@ -0,0 +1,24 @@
+﻿namespace DistributedStorageTests.Storage
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FileSliceContainerClass
+    {
+        [TestClass]
+        public class GetKeysMethod
+        { }
+
+        [TestClass]
+        public class TryAddMethod
+        { }
+
+        [TestClass]
+        public class TryGetMethod
+        { }
+
+        [TestClass]
+        public class TryRemoveMethod
+        { }
+    }
+}
