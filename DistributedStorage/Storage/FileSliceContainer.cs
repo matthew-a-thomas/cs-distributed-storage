e5437dcd6d0a922764615daf72ed8ada1768da25
Matthew Thomas <me@matthewathomas.com>
2017-06-08 09:50:39 -0400

    Broke FileSliceContainer out as a separate class

---
+++ DistributedStorage/Storage/FileSliceContainer.cs
@@ -0,0 +1,80 @@
+﻿namespace DistributedStorage.Storage
+{
+    using System.Collections.Generic;
+    using Common;
+    using Encoding;
+    using IFolder = IFactoryContainer<string, IFile>;
+
+    public sealed class FileSliceContainer : IContainer<Hash, Slice>
+    {
+        private readonly IFolder _workingFolder;
+
+        public FileSliceContainer(IFolder workingFolder)
+        {
+            _workingFolder = workingFolder;
+        }
+
+        public IEnumerable<Hash> GetKeys()
+        {
+            foreach (var key in _workingFolder.GetKeys())
+            {
+                if (!key.TryToBytes(out var hashBytes))
+                    continue;
+                Hash hash;
+                try
+                {
+                    hash = new Hash(hashBytes);
+                }
+                catch
+                {
+                    continue;
+                }
+                yield return hash;
+            }
+        }
+
+        private static string HashToString(Hash hash) => hash.HashCode.ToHex();
+        
+        public bool TryAdd(Hash hash, Slice slice)
+        {
+            try
+            {
+                if (!_workingFolder.TryCreate(HashToString(hash), out var file))
+                    return false;
+                if (!file.TryOpenWrite(out var stream))
+                    return false;
+                using (stream)
+                {
+                    stream.Write(slice);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        public bool TryGet(Hash hash, out Slice slice)
+        {
+            slice = null;
+            try
+            {
+                if (!_workingFolder.TryGet(HashToString(hash), out var file))
+                    return false;
+                if (!file.TryOpenRead(out var stream))
+                    return false;
+                using (stream)
+                {
+                    return stream.TryImmediateRead(out slice);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        public bool TryRemove(Hash hash) => _workingFolder.TryRemove(HashToString(hash));
+    }
+}
--- DistributedStorage/Storage/FileSliceContainerFactory.cs
+++ DistributedStorage/Storage/FileSliceContainerFactory.cs
@@ -1,11 +1,10 @@
 ﻿namespace DistributedStorage.Storage
 {
-    using System.Collections.Generic;
     using Common;
     using Encoding;
     using IFolder = IFactoryContainer<string, IFile>;
 
-    public sealed class FileSliceContainerFactory : ISliceContainerFactory
+    public sealed class FileSliceContainerFactory
     {
         private readonly IFactoryContainer<string, IFolder> _baseContainer;
 
@@ -14,86 +13,14 @@
             _baseContainer = baseContainer;
         }
 
-        public IContainer<Hash, Slice> CreateSliceContainer(Manifest forManifest)
+        public FileSliceContainer CreateSliceContainer(Manifest forManifest)
         {
             // Create a subdirectory for slices connected to this manifest
-            var workingDirectory = _baseContainer.GetOrCreate(forManifest.Id.HashCode.ToHex());
+            var workingFolder = _baseContainer.GetOrCreate(forManifest.Id.HashCode.ToHex());
 
-            // Set up the GetKeys function
-            IEnumerable<Hash> GetKeys()
-            {
-                foreach (var key in workingDirectory.GetKeys())
-                {
-                    if (!key.TryToBytes(out var hashBytes))
-                        continue;
-                    Hash hash;
-                    try
-                    {
-                        hash = new Hash(hashBytes);
-                    }
-                    catch
-                    {
-                        continue;
-                    }
-                    yield return hash;
-                }
-            }
-
-            // Set up the TryAdd function
-            bool TryAdd(Hash hash, Slice slice)
-            {
-                try
-                {
-                    if (!workingDirectory.TryCreate(HashToString(hash), out var file))
-                        return false;
-                    if (!file.TryOpenWrite(out var stream))
-                        return false;
-                    using (stream)
-                    {
-                        stream.Write(slice);
-                        return true;
-                    }
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-
-            // Set up the TryGet function
-            bool TryGet(Hash hash, out Slice slice)
-            {
-                slice = null;
-                try
-                {
-                    if (!workingDirectory.TryGet(HashToString(hash), out var file))
-                        return false;
-                    if (!file.TryOpenRead(out var stream))
-                        return false;
-                    using (stream)
-                    {
-                        return stream.TryImmediateRead(out slice);
-                    }
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-
-            // Set up the TryRemove function
-            bool TryRemove(Hash hash) => workingDirectory.TryRemove(HashToString(hash));
-
-            // Return a new IContainer using the above functions
-            return new Container<Hash, Slice>(new Container<Hash, Slice>.Options
-            {
-                GetKeys = GetKeys,
-                TryAdd = TryAdd,
-                TryGet = TryGet,
-                TryRemove = TryRemove
-            });
+            // Return a new FileSliceContainer
+            return new FileSliceContainer(workingFolder);
         }
 
-        private static string HashToString(Hash hash) => hash.HashCode.ToHex();
     }
 }
