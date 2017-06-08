56da3e3c9eedb5f74cc63b568329d0c80e27cb98
Matthew Thomas <me@matthewathomas.com>
2017-06-08 09:44:35 -0400

    Added IFactoryContainer, which knows how to create new contained elements

---
+++ DistributedStorage/Storage/IFactoryContainer.cs
@@ -0,0 +1,7 @@
+﻿namespace DistributedStorage.Storage
+{
+    public interface IFactoryContainer<TKey, TValue> : IContainer<TKey, TValue>
+    {
+        bool TryCreate(TKey key, out TValue value);
+    }
+}
