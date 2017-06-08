57a9e8ada4cd5d3531de91204acaeb9e31f0c1b2
Matthew Thomas <me@matthewathomas.com>
2017-06-08 10:13:56 -0400

    WIP: working on file slice container tests

--- DistributedStorageTests/Storage/FileSliceContainerClass.cs
+++ DistributedStorageTests/Storage/FileSliceContainerClass.cs
@@ -1,10 +1,25 @@
 ﻿namespace DistributedStorageTests.Storage
 {
+    using DistributedStorage.Storage;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class FileSliceContainerClass
     {
+        private static void CreateContainer(out FileSliceContainer container, out IFactoryContainer<string, IFile> folder)
+        {
+            folder = new FactoryContainer<string, IFile>(
+                container: new Container<string, IFile>(new Container<string, IFile>.Options
+                {
+                    GetKeys
+                }),
+                options: new FactoryContainer<string, IFile>.Options
+                {
+
+                });
+            container = new FileSliceContainer(workingFolder: folder);
+        }
+
         [TestClass]
         public class GetKeysMethod
         { }
