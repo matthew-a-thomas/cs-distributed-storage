19e4a7096dc2982aa2d47ae9412c243ace2d035b
Matthew Thomas <me@matthewathomas.com>
2017-06-08 09:06:05 -0400

    Replaces FileContainer with FileSliceContainerFactory

--- DistributedStorage/Storage/FileSliceContainerFactory.cs
+++ DistributedStorage/Storage/FileSliceContainerFactory.cs
@@ -1,45 +1,103 @@
-﻿namespace DistributedStorage.Storage
+﻿using System.Collections.Generic;
+
+namespace DistributedStorage.Storage
 {
     using System.IO;
-    using System.Linq;
     using Common;
     using Encoding;
 
     public sealed class FileSliceContainerFactory : ISliceContainerFactory
     {
-        private readonly DirectoryInfo _workingDirectory;
+        private readonly DirectoryInfo _baseDirectory;
 
-        public FileSliceContainerFactory(DirectoryInfo workingDirectory)
+        public FileSliceContainerFactory(DirectoryInfo baseDirectory)
         {
-            _workingDirectory = workingDirectory;
+            _baseDirectory = baseDirectory;
         }
 
         public IContainer<Hash, Slice> CreateSliceContainer(Manifest forManifest)
         {
-            var fileContainer = new FileContainer<Slice>(
-                deserializer: bytes =>
+            // Create a subdirectory for slices connected to this manifest
+            var workingDirectory = _baseDirectory.CreateSubdirectory(forManifest.Id.HashCode.ToHex());
+
+            // Set up the GetKeys function
+            IEnumerable<Hash> GetKeys()
+            {
+                foreach (var file in workingDirectory.EnumerateFiles())
                 {
-                    using (var stream = new MemoryStream(bytes))
+                    if (!file.Name.TryToBytes(out var hashBytes))
+                        continue;
+                    Hash hash;
+                    try
                     {
-                        return stream.TryImmediateRead(out Slice slice) ? slice : null;
+                        hash = new Hash(hashBytes);
                     }
-                },
-                serializer: slice =>
+                    catch
+                    {
+                        continue;
+                    }
+                    yield return hash;
+                }
+            }
+
+            // Set up the TryAdd function
+            bool TryAdd(Hash hash, Slice slice)
+            {
+                try
                 {
-                    using (var stream = new MemoryStream())
+                    using (var stream = GetFileInfoFor(workingDirectory, hash).Open(FileMode.CreateNew, FileAccess.Write, FileShare.None))
                     {
                         stream.Write(slice);
-                        return stream.ToArray();
+                        return true;
                     }
-                },
-                workingDirectory: new DirectoryInfo(Path.Combine(_workingDirectory.FullName, forManifest.Id.HashCode.ToHex()))
-                );
+                }
+                catch
+                {
+                    return false;
+                }
+            }
 
+            // Set up the TryGet function
+            bool TryGet(Hash hash, out Slice slice)
+            {
+                try
+                {
+                    using (var stream = GetFileInfoFor(workingDirectory, hash).Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        return stream.TryImmediateRead(out slice);
+                    }
+                }
+                catch
+                {
+                    slice = null;
+                    return false;
+                }
+            }
+
+            // Set up the TryRemove function
+            bool TryRemove(Hash hash)
+            {
+                var fileInfo = GetFileInfoFor(workingDirectory, hash);
+                var exists = fileInfo.Exists;
+                if (exists)
+                    fileInfo.Delete();
+                return exists;
+            }
+
+            // Return a new IContainer using the above functions
             return new Container<Hash, Slice>(new Container<Hash, Slice>.Options
             {
-                GetKeys = () => fileContainer.GetKeys().Select(Hash.Create),
-                TryAdd = (hash, slice) => fileContainer.TryAdd(hash.HashCode, slice.ToBytes())
+                GetKeys = GetKeys,
+                TryAdd = TryAdd,
+                TryGet = TryGet,
+                TryRemove = TryRemove
             });
         }
+
+        /// <summary>
+        /// Creates a <see cref="FileInfo"/> that should be used for the given <paramref name="hash"/>
+        /// </summary>
+        // ReSharper disable once SuggestBaseTypeForParameter
+        private static FileInfo GetFileInfoFor(DirectoryInfo workingDirectory, Hash hash) => new FileInfo(Path.Combine(workingDirectory.FullName, hash.HashCode.ToHex()));
     }
 }
