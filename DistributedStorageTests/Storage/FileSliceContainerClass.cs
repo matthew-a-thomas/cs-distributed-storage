261534a6c5bab6a405d0a291f4e2b26b01fc9f2c
Matthew Thomas <me@matthewathomas.com>
2017-06-08 13:29:51 -0400

    Additional unit tests

--- DistributedStorageTests/Storage/FileSliceContainerClass.cs
+++ DistributedStorageTests/Storage/FileSliceContainerClass.cs
@@ -5,6 +5,7 @@
     using System.IO;
     using System.Linq;
     using DistributedStorage.Common;
+    using DistributedStorage.Encoding;
     using DistributedStorage.Storage;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using File = DistributedStorage.Storage.File;
@@ -21,18 +22,8 @@
             bool TryCreate(string key, out IFile file)
             {
                 if (dictionaryContainer.TryGet(key, out file))
-                    return true;
-                var buffer = new byte[1024 * 10];
-                bool TryOpen(out Stream stream)
-                {
-                    stream = new MemoryStream(buffer);
-                    return true;
-                }
-                file = new File(new File.Options
-                {
-                    TryOpenRead = TryOpen,
-                    TryOpenWrite = TryOpen
-                });
+                    return false;
+                file = CreateFile();
                 return dictionaryContainer.TryAdd(key, file);
             }
             folder = new FactoryContainer<string, IFile>(dictionaryContainer, new FactoryContainer<string, IFile>.Options
@@ -41,6 +32,32 @@
             });
             container = new FileSliceContainer(folder);
         }
+
+        /// <summary>
+        /// Creates a new <see cref="IFile"/> that wraps an internal byte array
+        /// </summary>
+        private static IFile CreateFile()
+        {
+            var buffer = new byte[1024 * 10];
+
+            bool TryOpen(out Stream stream)
+            {
+                stream = new MemoryStream(buffer);
+                return true;
+            }
+
+            return new File(new File.Options
+            {
+                TryOpenRead = TryOpen,
+                TryOpenWrite = TryOpen
+            });
+        }
+
+        private static Slice CreateSlice() => new Slice
+        {
+            Coefficients = new[] {true, false, false},
+            EncodingSymbol = new byte[] {0x00, 0x01, 0x02}
+        };
 
         [TestClass]
         public class GetKeysMethod
@@ -67,7 +84,20 @@
         public class TryAddMethod
         {
             [TestMethod]
-            public void Implemented() => throw new NotImplementedException();
+            public void ReturnsFalseForDuplicateHash()
+            {
+                CreateContainerAndFolder(out var container, out var folder);
+                var hash = Hash.Create(System.Text.Encoding.ASCII.GetBytes("Hello world"));
+                Assert.IsTrue(folder.TryAdd(hash.HashCode.ToHex(), CreateFile()));
+                Assert.IsFalse(container.TryAdd(hash, CreateSlice()));
+            }
+
+            [TestMethod]
+            public void ReturnsTrueForUniqueHash()
+            {
+                CreateContainerAndFolder(out var container, out _);
+                Assert.IsTrue(container.TryAdd(Hash.Create(new byte[0]), CreateSlice()));
+            }
         }
 
         [TestClass]
