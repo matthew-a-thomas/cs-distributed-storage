1d68847b571882c5ba2ddaec8c681ca0acdc35f3
Matthew Thomas <me@matthewathomas.com>
2017-06-09 15:05:59 -0400

    WIP: beginnings of storage and factory for console app

---
+++ ConsoleApp/Storage.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp
+{
+    using System.Security.Cryptography;
+    using DistributedStorage.Common;
+    using DistributedStorage.Encoding;
+    using DistributedStorage.Storage;
+
+    internal sealed class Storage
+    {
+        public IFactoryContainer<Manifest, IContainer<Hash, Slice>> ContainersForManifests { get; set; }
+
+        public IFile OurRsaKey { get; set; }
+
+        public IContainer<Hash, RSAParameters> TrustedPublicKeys { get; set; }
+    }
+}
---
+++ ConsoleApp/StorageFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp
+{
+    using DistributedStorage.Storage;
+
+    internal sealed class StorageFactory
+    {
+        public sealed class Dependencies
+        {
+            public IFactoryContainer<string, IFactoryContainer<string, IFile>> WorkingDirectory { get; }
+
+            public Dependencies(IFactoryContainer<string, IFactoryContainer<string, IFile>> workingDirectory)
+            {
+                WorkingDirectory = workingDirectory;
+            }
+        }
+
+        public sealed class Options
+        {
+            public string OurRsaKeyName { get; set; } = "ours.rsa";
+            public string OurRsaFolderName { get; set; } = "private";
+            public string ManifestsFolderName { get; set; } = "manifests";
+            public string TrustedPublicKeysFolderName { get; set; } = "trusted";
+        }
+
+        private readonly Dependencies _dependencies;
+
+        public StorageFactory(Dependencies dependencies)
+        {
+            _dependencies = dependencies;
+        }
+
+        public Storage CreateStorage(Options options = null)
+        {
+            options = options ?? new Options();
+            return new Storage
+            {
+                
+            }
+        }
+    }
+}
