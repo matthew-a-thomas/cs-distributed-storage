e9e5b50ddbdf35f73ee82bfa86dfe918521536d1
Matthew Thomas <me@matthewathomas.com>
2017-06-10 09:18:09 -0400

    Added unit test to test count of keys after addition of one

--- DistributedStorageTests/Storage/PublicKeysContainerClass.cs
+++ DistributedStorageTests/Storage/PublicKeysContainerClass.cs
@@ -1,12 +1,16 @@
 ﻿namespace DistributedStorageTests.Storage
 {
     using System.Linq;
+    using DistributedStorage.Networking.Security;
     using DistributedStorage.Storage;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Networking.Security;
 
     [TestClass]
     public class PublicKeysContainerClass
     {
+        private static readonly RsaKeyProvider KeyProvider = new RsaKeyProvider();
+
         private static PublicKeysContainer Create() => new PublicKeysContainer(new PublicKeysContainer.Options(".rsa", Helpers.CreateDirectory()));
 
         [TestClass]
@@ -17,6 +21,14 @@
             {
                 var container = Create();
                 Assert.AreEqual(0, container.GetKeys().Count());
+            }
+
+            [TestMethod]
+            public void ReturnsSomethingAfterOneIsAdded()
+            {
+                var container = Create();
+                Assert.IsTrue(container.TryAdd(KeyProvider.RsaKey1.ToHash(), KeyProvider.RsaKey1));
+                Assert.AreEqual(1, container.GetKeys().Count());
             }
         }
     }
