1e4ca8345317ce75eb601f63c8f50f812b511178
Matthew Thomas <me@matthewathomas.com>
2017-06-10 09:40:59 -0400

    Added quick unit test for StorageFactory class

---
+++ DistributedStorageTests/Storage/StorageFactoryClass.cs
@@ -0,0 +1,39 @@
+﻿namespace DistributedStorageTests.Storage
+{
+    using System.IO;
+    using DistributedStorage.Storage;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StorageFactoryClass
+    {
+        [TestClass]
+        public class CreateMethod
+        {
+            [TestClass]
+            public class ReturnValue
+            {
+                [TestClass]
+                public class OurRsaKeyFileProperty
+                {
+                    [TestMethod]
+                    public void CanBeReadFromAndWrittenToAndSeeked()
+                    {
+                        var storage = new StorageFactory().CreateStorage(Helpers.CreateDirectory());
+                        Assert.IsTrue(storage.OurRsaKeyFile.TryOpenRead(out var stream));
+                        using (stream)
+                        {
+                            Assert.IsTrue(stream.CanRead);
+                            Assert.IsTrue(stream.CanSeek);
+                            Assert.IsTrue(stream.CanWrite);
+                            stream.WriteByte(0x12);
+                            stream.Seek(0, SeekOrigin.Begin);
+                            var b = stream.ReadByte();
+                            Assert.AreEqual(0x12, b);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
