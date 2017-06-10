c72e0787f50b23e8e6aa9f37dcbd65f65b24c52a
Matthew Thomas <me@matthewathomas.com>
2017-06-10 15:49:24 -0400

    Finished unit tests for AuthorizedCommandManager

--- DistributedStorageTests/Actors/AuthorizedCommandManagerClass.cs
+++ DistributedStorageTests/Actors/AuthorizedCommandManagerClass.cs
@@ -3,6 +3,7 @@
     using DistributedStorage.Actors;
     using DistributedStorage.Networking.Security;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Networking.Security.Utils;
 
     [TestClass]
     public class AuthorizedCommandManagerClass
@@ -41,6 +42,73 @@
                 Assert.IsTrue(manager.TryAuthorize(x => parameter = x, out var token));
                 manager.Invoke(token, 0);
                 Assert.AreEqual(0, parameter);
+            }
+        }
+
+        [TestClass]
+        public class TryAuthorizeMethod
+        {
+            [TestMethod]
+            public void ReturnsFalseForDuplicatedTokenThatHasNotBeenUsed()
+            {
+                var manager = new AuthorizedCommandManager<int>(new NonsecureEntropy(), 1);
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out _));
+                Assert.IsFalse(manager.TryAuthorize(_ => { }, out _));
+            }
+
+            [TestMethod]
+            public void ReturnsTrueForDuplicatedTokenThatHasBeenUsed()
+            {
+                var manager = new AuthorizedCommandManager<int>(new NonsecureEntropy(), 1);
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out var token));
+                manager.Invoke(token, 0);
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out _));
+            }
+
+            [TestMethod]
+            public void ReturnsTrueForUniqueToken()
+            {
+                var manager = Create();
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out _));
+            }
+        }
+
+        [TestClass]
+        public class TryUnauthorizeMethod
+        {
+            [TestMethod]
+            public void MakesTryAuthorizeReturnTrueForSameToken()
+            {
+                var manager = new AuthorizedCommandManager<int>(new NonsecureEntropy(), 1);
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out var token));
+                Assert.IsTrue(manager.TryUnauthorize(token));
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out _));
+            }
+
+            [TestMethod]
+            public void ReturnsFalseWhenNoAuthorizations()
+            {
+                var manager = Create();
+                Assert.IsFalse(manager.TryUnauthorize(new byte[0]));
+            }
+
+            [TestMethod]
+            public void ReturnsTrueForAuthorization()
+            {
+                var manager = Create();
+                Assert.IsTrue(manager.TryAuthorize(_ => { }, out var token));
+                Assert.IsTrue(manager.TryUnauthorize(token));
+            }
+
+            [TestMethod]
+            public void StopsActionFromBeingInvoked()
+            {
+                var manager = Create();
+                var invoked = false;
+                Assert.IsTrue(manager.TryAuthorize(_ => invoked = true, out var token));
+                Assert.IsTrue(manager.TryUnauthorize(token));
+                manager.Invoke(token, 0);
+                Assert.IsFalse(invoked);
             }
         }
     }
