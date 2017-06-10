0aa76a046a8f53b9c0ef3ab07e355c821a4cf6de
Matthew Thomas <me@matthewathomas.com>
2017-06-10 09:10:41 -0400

    Started unit tests for PublicKeysContainer class

---
+++ DistributedStorageTests/Storage/PublicKeysContainerClass.cs
@@ -0,0 +1,23 @@
+﻿namespace DistributedStorageTests.Storage
+{
+    using System.Linq;
+    using DistributedStorage.Storage;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PublicKeysContainerClass
+    {
+        private static PublicKeysContainer Create() => new PublicKeysContainer(new PublicKeysContainer.Options(".rsa", Helpers.CreateDirectory()));
+
+        [TestClass]
+        public class TryGetKeysMethod
+        {
+            [TestMethod]
+            public void ReturnsNothingForNewInstance()
+            {
+                var container = Create();
+                Assert.AreEqual(0, container.GetKeys().Count());
+            }
+        }
+    }
+}
