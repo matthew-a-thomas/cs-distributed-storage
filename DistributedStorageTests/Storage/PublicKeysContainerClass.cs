3c98a032e2001d187db96e3432b19b0e97d4b8b3
Matthew Thomas <me@matthewathomas.com>
2017-06-10 09:26:19 -0400

    Finished unit tests for this class

--- DistributedStorageTests/Storage/PublicKeysContainerClass.cs
+++ DistributedStorageTests/Storage/PublicKeysContainerClass.cs
@@ -36,6 +36,20 @@
         public class TryAddMethod
         {
             [TestMethod]
+            public void DoesNotAddPrivateKey()
+            {
+                var container = Create();
+                Assert.IsTrue(container.TryAdd(KeyProvider.RsaKey1.ToHash(), KeyProvider.RsaKey1));
+                Assert.IsTrue(container.TryGet(KeyProvider.RsaKey1.ToHash(), out var key));
+                Assert.IsNull(key.D);
+                Assert.IsNull(key.DP);
+                Assert.IsNull(key.DQ);
+                Assert.IsNull(key.InverseQ);
+                Assert.IsNull(key.P);
+                Assert.IsNull(key.Q);
+            }
+
+            [TestMethod]
             public void ReturnsFalseForExistentThing()
             {
                 var container = Create();
@@ -68,6 +82,26 @@
                 Assert.IsTrue(container.TryAdd(KeyProvider.RsaKey1.ToHash(), KeyProvider.RsaKey1));
                 Assert.IsTrue(container.TryGet(KeyProvider.RsaKey1.ToHash(), out var key));
                 Assert.IsTrue(key.ToBytes().SequenceEqual(KeyProvider.RsaKey1.ToBytes()));
+            }
+        }
+
+        [TestClass]
+        public class TryRemoveMethod
+        {
+            [TestMethod]
+            public void RemovesPreviouslyAddedThing()
+            {
+                var container = Create();
+                Assert.IsTrue(container.TryAdd(KeyProvider.RsaKey1.ToHash(), KeyProvider.RsaKey1));
+                Assert.IsTrue(container.TryRemove(KeyProvider.RsaKey1.ToHash()));
+                Assert.IsFalse(container.TryGet(KeyProvider.RsaKey1.ToHash(), out _));
+            }
+
+            [TestMethod]
+            public void ReturnsFalseForNonexistentThing()
+            {
+                var container = Create();
+                Assert.IsFalse(container.TryRemove(KeyProvider.RsaKey1.ToHash()));
             }
         }
     }
