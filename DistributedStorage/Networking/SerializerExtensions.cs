ca5551a51179a799f1cc2d37bb817fd43c73042e
Matthew Thomas <me@matthewathomas.com>
2017-06-13 21:26:37 -0400

    Added extension method to create a pair of converters from an ISerializer. One Converter converts to byte arrays; the other converts from byte arrays

---
+++ DistributedStorage/Networking/SerializerExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace DistributedStorage.Networking
+{
+    using Common;
+
+    public static class SerializerExtensions
+    {
+        /// <summary>
+        /// Returns a pair of <see cref="IConverter{TFrom, TTo}"/>s that use the given <see cref="ISerializer{T}"/>
+        /// </summary>
+        public static (IConverter<T, byte[]> toByteArray, IConverter<byte[], T> fromByteArray) ToConverters<T>(this ISerializer<T> serializer)
+        {
+            bool TryConvertFromBytes(byte[] bytes, out T thing) => serializer.TryDeserialize(bytes, out thing);
+            bool TryConvertToBytes(T thing, out byte[] bytes)
+            {
+                bytes = serializer.Serialize(thing);
+                return true;
+            }
+            return (
+                new Converter<T, byte[]>(TryConvertToBytes),
+                new Converter<byte[], T>(TryConvertFromBytes)
+            );
+        }
+    }
+}
