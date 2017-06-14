d6e3a198529bf8ec2de0410bc0bc9d1d01017a1a
Matthew Thomas <me@matthewathomas.com>
2017-06-13 22:25:23 -0400

    Added extension method to generate random integers from an IEntropy

---
+++ DistributedStorage/Networking/Security/EntropyExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributedStorage.Networking.Security
+{
+    using System;
+
+    public static class EntropyExtensions
+    {
+        /// <summary>
+        /// Creates a random <see cref="int"/> from this <see cref="IEntropy"/>
+        /// </summary>
+        public static int NextInteger(this IEntropy entropy) => BitConverter.ToInt32(entropy.CreateNonce(4), 0);
+    }
+}
