7d71492da7d94b5a1b3b5ab22075782f9d3e3d78
Matthew Thomas <me@matthewathomas.com>
2017-06-13 22:26:23 -0400

    Added TieBreaker, which knows how to deal with ties between two parties. It can resolve ties eventually through enough calls to .Test() and with good enough entropy.
    Also added tests

---
+++ DistributedStorage/Common/TieBreaker.cs
@@ -0,0 +1,66 @@
+﻿namespace DistributedStorage.Common
+{
+    using System;
+    using Networking.Security;
+
+    /// <summary>
+    /// Something that knows how to deal with ties
+    /// </summary>
+    public sealed class TieBreaker
+    {
+        /// <summary>
+        /// The result of trying to break a tie
+        /// </summary>
+        public enum Result
+        {
+            /// <summary>
+            /// You won the tie break
+            /// </summary>
+            YouWon,
+
+            /// <summary>
+            /// They won the tie break
+            /// </summary>
+            TheyWon,
+
+            /// <summary>
+            /// You both tied
+            /// </summary>
+            Tie
+        }
+
+        private readonly IEntropy _entropy;
+        private readonly Action<int> _sendOurValue;
+        private readonly Func<int> _getTheirValue;
+
+        /// <summary>
+        /// Creates a new <see cref="TieBreaker"/>, which knows how to deal with ties
+        /// </summary>
+        /// <param name="entropy">Your source of entropy, from which random numbers will be drawn</param>
+        /// <param name="sendOurValue">Your way of communicating your number to the other party</param>
+        /// <param name="getTheirValue">Your way of receiving a number from the other party</param>
+        public TieBreaker(IEntropy entropy, Action<int> sendOurValue, Func<int> getTheirValue)
+        {
+            _entropy = entropy;
+            _sendOurValue = sendOurValue;
+            _getTheirValue = getTheirValue;
+        }
+
+        /// <summary>
+        /// Breaks a tie by generating a random number, swapping it with the other party (through the methods given to the constructor), and comparing your number with theirs.
+        /// The party with the larger number wins, and it's a tie if both numbers are the same size
+        /// </summary>
+        public Result Test()
+        {
+            var ourValue = _entropy.NextInteger();
+            _sendOurValue(ourValue);
+            var theirValue = _getTheirValue();
+            return
+                ourValue > theirValue
+                ? Result.YouWon
+                : (theirValue > ourValue
+                ? Result.TheyWon
+                : Result.Tie);
+        }
+    }
+}
---
+++ DistributedStorageTests/Common/TieBreakerClass.cs
@@ -0,0 +1,42 @@
+﻿namespace DistributedStorageTests.Common
+{
+    using DistributedStorage.Common;
+    using DistributedStorage.Networking.Security;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Networking.Security.Utils;
+
+    [TestClass]
+    public class TieBreakerClass
+    {
+        [TestClass]
+        public class TestMethod
+        {
+            [TestMethod]
+            public void TheyWinWhenTheirNumberIsLarger()
+            {
+                var entropy = new NonsecureEntropy();
+                var theirValue = entropy.NextInteger() + 1;
+                var tieBreaker = new TieBreaker(entropy, _ => { }, () => theirValue);
+                Assert.AreEqual(TieBreaker.Result.TheyWon, tieBreaker.Test());
+            }
+
+            [TestMethod]
+            public void ReturnsTieWhenBothPartiesGenerateSameNumber()
+            {
+                var entropy = new NonsecureEntropy();
+                var theirValue = entropy.NextInteger();
+                var tieBreaker = new TieBreaker(entropy, _ => { }, () => theirValue);
+                Assert.AreEqual(TieBreaker.Result.Tie, tieBreaker.Test());
+            }
+
+            [TestMethod]
+            public void YouWinWhenYourNumberIsLarger()
+            {
+                var entropy = new NonsecureEntropy();
+                var theirValue = entropy.NextInteger() - 1;
+                var tieBreaker = new TieBreaker(entropy, _ => { }, () => theirValue);
+                Assert.AreEqual(TieBreaker.Result.YouWon, tieBreaker.Test());
+            }
+        }
+    }
+}
