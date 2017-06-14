21a3f0a05f1ca537812c06f01b3097f95aca7bc6
Matthew Thomas <me@matthewathomas.com>
2017-06-13 20:59:35 -0400

    Added extension methods to Task

---
+++ DistributedStorage/Common/TaskExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace DistributedStorage.Common
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Determines if thie <see cref="Task"/> is completed successfully
+        /// </summary>
+        public static bool IsSuccessfullyCompleted(this Task task) => task.IsCompleted && !task.IsCanceled && !task.IsFaulted;
+
+        /// <summary>
+        /// Schedules the given <see cref="Action"/> to execute when this <see cref="Task"/> is completed successfully
+        /// </summary>
+        public static void DoAfterSuccess(this Task task, Action success) => task.ContinueWith(t =>
+        {
+            if (!t.IsSuccessfullyCompleted())
+                return;
+            success();
+        });
+
+        /// <summary>
+        /// Schedules the given <see cref="Action"/> to execute when this <see cref="Task"/> is completed successfully
+        /// </summary>
+        public static void DoAfterSuccess<T>(this Task<T> task, Action<T> success) => task.ContinueWith(t =>
+        {
+            if (!t.IsSuccessfullyCompleted())
+                return;
+            success(t.Result);
+        });
+    }
+}
