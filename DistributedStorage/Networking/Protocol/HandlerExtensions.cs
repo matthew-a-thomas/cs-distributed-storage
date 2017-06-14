3066818684103844abb571662a900f8100eb97ed
Matthew Thomas <me@matthewathomas.com>
2017-06-13 21:25:14 -0400

    Added extension method to convert from one kind of handler to another

---
+++ DistributedStorage/Networking/Protocol/HandlerExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace DistributedStorage.Networking.Protocol
+{
+    using Common;
+
+    public static class HandlerExtensions
+    {
+        /// <summary>
+        /// Uses a couple of <see cref="IConverter{TFrom, TTo}"/>s to convert this <see cref="IHandler{TParameter, TResult}"/> into a different one
+        /// </summary>
+        public static IHandler<TParameterTo, TResultTo> To<TParameterFrom, TResultFrom, TParameterTo, TResultTo>(
+            this IHandler<TParameterFrom, TResultFrom> handler,
+            IConverter<TParameterTo, TParameterFrom> parameterConverter,
+            IConverter<TResultFrom, TResultTo> resultConverter
+        ) => new Handler<TParameterTo, TResultTo>(parameter =>
+        {
+            if (!parameterConverter.TryConvert(parameter, out var convertedParameter))
+                return default(TResultTo);
+            var result = handler.Handle(convertedParameter);
+            resultConverter.TryConvert(result, out var convertedResult);
+            return convertedResult;
+        });
+    }
+}
