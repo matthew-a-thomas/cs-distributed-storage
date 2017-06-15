c9f21ff0d4190c3bb3eb41757b46c76d38db39b8
Matthew Thomas <me@matthewathomas.com>
2017-06-14 22:18:51 -0400

    Added extension method for a blocking Invoke

---
+++ DistributedStorage/Actors/DispatcherExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace DistributedStorage.Actors
+{
+    using System;
+    using System.Threading;
+
+    public static class DispatcherExtensions
+    {
+        /// <summary>
+        /// Invokes the given <paramref name="action"/> on this <see cref="IDispatcher"/>, blocking this thread until it is complete
+        /// </summary>
+        public static void Invoke(this IDispatcher dispatcher, Action action)
+        {
+            var gate = new ManualResetEventSlim();
+            dispatcher.BeginInvoke(() =>
+            {
+                try
+                {
+                    action();
+                }
+                finally
+                {
+                    gate.Set();
+                }
+            });
+            gate.Wait();
+        }
+    }
+}
