a1cef3a3facb3743da84c6c3bbe19d4f387f0d39
Matthew Thomas <me@matthewathomas.com>
2017-06-14 21:57:01 -0400

    Added unit tests for WorkQueue class

---
+++ DistributedStorageTests/Actors/WorkQueueClass.cs
@@ -0,0 +1,31 @@
+﻿namespace DistributedStorageTests.Actors
+{
+    using DistributedStorage.Actors;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WorkQueueClass
+    {
+        [TestClass]
+        public class EnqueueMethod
+        {
+            [TestMethod]
+            public void InvokesCallback()
+            {
+                var invoked = false;
+                var queue = new WorkQueue<int>(_ => invoked = true);
+                queue.Enqueue(0);
+                Assert.IsTrue(invoked);
+            }
+
+            [TestMethod]
+            public void PassesNodeWithGivenValue()
+            {
+                var receivedValue = -1;
+                var queue = new WorkQueue<int>(node => receivedValue = node.Value);
+                queue.Enqueue(5);
+                Assert.AreEqual(5, receivedValue);
+            }
+        }
+    }
+}
