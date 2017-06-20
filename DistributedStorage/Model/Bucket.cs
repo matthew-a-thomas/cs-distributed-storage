9ab4814b3eda1f71fde0f3b8bf85f0deed1ac83f
Matthew Thomas <me@matthewathomas.com>
2017-06-20 19:01:59 -0400

    Fixed to match interfaces

--- DistributedStorage/Model/Bucket.cs
+++ DistributedStorage/Model/Bucket.cs
@@ -1,19 +1,54 @@
 ﻿
 namespace DistributedStorage.Model
 {
+    using System.Collections.Generic;
+    using Common;
+    using Encoding;
+
     public sealed class Bucket : IBucket
     {
-        public Bucket(IIdentity identity, IIdentity owner, IIdentity pool, long size)
+        public delegate IEnumerable<Hash> GetHashesDelegate(Manifest forManifest);
+        public delegate IEnumerable<Manifest> GetManifestsDelegate();
+        public delegate IEnumerable<Slice> GetSlicesDelegate(Manifest forManifest, Hash[] hashes);
+
+        public sealed class Options
         {
-            SelfIdentity = identity;
-            OwnerIdentity = owner;
-            PoolIdentity = pool;
-            Size = size;
+            public GetHashesDelegate GetHashes { get; }
+            public GetManifestsDelegate GetManifests { get; }
+            public GetSlicesDelegate GetSlices { get; }
+            public IIdentity OwnerIdentity { get; }
+            public IIdentity PoolIdentity { get; }
+            public IIdentity SelfIdentity { get; }
+            public long Size { get; }
+
+            public Options(GetHashesDelegate getHashes, GetManifestsDelegate getManifests, GetSlicesDelegate getSlices, IIdentity ownerIdentity, IIdentity poolIdentity, IIdentity selfIdentity, long size)
+            {
+                GetHashes = getHashes;
+                GetManifests = getManifests;
+                GetSlices = getSlices;
+                OwnerIdentity = ownerIdentity;
+                PoolIdentity = poolIdentity;
+                SelfIdentity = selfIdentity;
+                Size = size;
+            }
         }
 
-        public IIdentity SelfIdentity { get; }
-        public IIdentity OwnerIdentity { get; }
-        public IIdentity PoolIdentity { get; }
-        public long Size { get; }
+        public IIdentity SelfIdentity => _options.SelfIdentity;
+        public IIdentity OwnerIdentity => _options.OwnerIdentity;
+        public IIdentity PoolIdentity => _options.PoolIdentity;
+        public long Size => _options.Size;
+
+        private readonly Options _options;
+
+        public Bucket(Options options)
+        {
+            _options = options;
+        }
+
+        public IEnumerable<Hash> GetHashes(Manifest forManifest) => _options.GetHashes(forManifest);
+
+        public IEnumerable<Manifest> GetManifests() => _options.GetManifests();
+
+        public IEnumerable<Slice> GetSlices(Manifest forManifest, Hash[] hashes) => _options.GetSlices(forManifest, hashes);
     }
 }
--- DistributedStorage/Model/PoolBucket.cs
+++ DistributedStorage/Model/PoolBucket.cs
@@ -1,7 +1,5 @@
 ﻿namespace DistributedStorage.Model
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using Common;
     using Encoding;
 
@@ -11,17 +9,11 @@
 
         public delegate void AddSlicesDelegate(Manifest forManifest, Slice[] slices);
         public delegate void DeleteSlicesDelegate(Manifest forManifest, Hash[] hashesToDelete);
-        public delegate IEnumerable<Hash> GetHashesDelegate(Manifest forManifest);
-        public delegate IEnumerable<Manifest> GetManifestsDelegate();
-        public delegate IEnumerable<Slice> GetSlicesDelegate(Manifest forManifest, Hash[] hashes);
 
         public sealed class Options
         {
             public AddSlicesDelegate AddSlices { get; set; } = (x, y) => { };
             public DeleteSlicesDelegate DeleteSlices { get; set; } = (x, y) => { };
-            public GetHashesDelegate GetHashes { get; set; } = x => Enumerable.Empty<Hash>();
-            public GetManifestsDelegate GetManifests { get; set; } = () => Enumerable.Empty<Manifest>();
-            public GetSlicesDelegate GetSlices { get; set; } = (x, y) => Enumerable.Empty<Slice>();
         }
 
         public PoolBucket(Options options)
@@ -32,11 +24,5 @@
         public void AddSlices(Manifest forManifest, Slice[] slices) => _options.AddSlices(forManifest, slices);
 
         public void DeleteSlices(Manifest forManifest, Hash[] hashesToDelete) => _options.DeleteSlices(forManifest, hashesToDelete);
-
-        public IEnumerable<Hash> GetHashes(Manifest forManifest) => _options.GetHashes(forManifest);
-
-        public IEnumerable<Manifest> GetManifests() => _options.GetManifests();
-
-        public IEnumerable<Slice> GetSlices(Manifest forManifest, Hash[] hashes) => _options.GetSlices(forManifest, hashes);
     }
 }
