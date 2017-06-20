46ca41f593d0daf5eab05c48bce9fb43408d8f80
Matthew Thomas <me@matthewathomas.com>
2017-06-20 19:28:47 -0400

    Added GetCurrentSize extension method to IDirectory

--- DistributedStorage/Storage/FileSystem/DirectoryExtensions.cs
+++ DistributedStorage/Storage/FileSystem/DirectoryExtensions.cs
@@ -1,9 +1,30 @@
 ﻿namespace DistributedStorage.Storage.FileSystem
 {
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+    using Containers;
 
     public static class DirectoryExtensions
     {
+        /// <summary>
+        /// Recursively calculates the total size of all files contained within this <see cref="IDirectory"/>
+        /// </summary>
+        public static long GetCurrentSize(this IDirectory directory)
+        {
+            var directories = new Stack<IDirectory>();
+            var total = 0L;
+            directories.Push(directory);
+            while (directories.Count > 0)
+            {
+                var d = directories.Pop();
+                total += d.Files.GetValues().Sum(file => file.GetCurrentSize());
+                foreach (var subdirectory in d.Directories.GetValues())
+                    directories.Push(subdirectory);
+            }
+            return total;
+        }
+
         /// <summary>
         /// Converts this <see cref="DirectoryInfo"/> into an <see cref="IDirectory"/>
         /// </summary>
