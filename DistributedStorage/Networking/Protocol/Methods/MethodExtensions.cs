c24ffa398bdafc8ef53f1c6218f12ebc0ce0e1b8
Matthew Thomas <me@matthewathomas.com>
2017-06-21 10:34:04 -0400

    Added InvokeAndWait extension method

---
+++ DistributedStorage/Networking/Protocol/Methods/MethodExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DistributedStorage.Networking.Protocol.Methods
+{
+    using System.Threading.Tasks;
+    using Common;
+
+    public static class MethodExtensions
+    {
+        public static TResult InvokeAndWait<TParameter, TResult>(this IMethod<TParameter, TResult> method, TParameter parameter)
+        {
+            var tcs = new TaskCompletionSource<TResult>();
+            method.Invoke(parameter, tcs.SetResult);
+            return tcs.Task.WaitAndGet();
+        }
+    }
+}
