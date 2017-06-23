c85688f61c86a4dd211ca4b98d468e89f5f3d387
Matthew Thomas <me@matthewathomas.com>
2017-06-23 18:57:25 -0400

    Added static method for creating Handlers from IConverters and a func<T,T>

--- DistributedStorage/Networking/Protocol/Handler.cs
+++ DistributedStorage/Networking/Protocol/Handler.cs
@@ -1,6 +1,7 @@
 ﻿namespace DistributedStorage.Networking.Protocol
 {
     using System;
+    using Common;
 
     public sealed class Handler<TParameter, TResult> : IHandler<TParameter, TResult>
     {
@@ -12,5 +13,22 @@
         }
 
         public TResult Handle(TParameter parameter) => _handler(parameter);
+    }
+
+    public static class Handler
+    {
+        public static Handler<TParameterIn, TResultOut> CreateFrom<TParameterIn, TParameterIntermediate, TResultIntermediate, TResultOut>(
+            IConverter<TParameterIn, TParameterIntermediate> parameterConverter,
+            IConverter<TResultIntermediate, TResultOut> resultConverter,
+            Func<TParameterIntermediate, TResultIntermediate> intermediateHandler
+        ) =>
+            new Handler<TParameterIn, TResultOut>(
+                parameterIn =>
+                parameterConverter.TryConvert(parameterIn, out var parameterIntermediate)
+                ? resultConverter.TryConvert(intermediateHandler(parameterIntermediate), out var resultOut)
+                ? resultOut
+                : default(TResultOut)
+                : default(TResultOut)
+                );
     }
 }
