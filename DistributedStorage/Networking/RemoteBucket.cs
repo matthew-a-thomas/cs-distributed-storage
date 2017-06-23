5c8285143f8c943bbfd9a56a80cc824b9812abc1
Matthew Thomas <me@matthewathomas.com>
2017-06-23 18:22:51 -0400

    Finished RemoteBucket

--- DistributedStorage/Networking/RemoteBucket.cs
+++ DistributedStorage/Networking/RemoteBucket.cs
@@ -9,15 +9,18 @@
     using Protocol.Methods;
     using Serialization;
 
+    /// <summary>
+    /// Implements the remote side of a networked <see cref="IBucket{TIdentity}"/>
+    /// </summary>
     public sealed class RemoteBucket<TIdentity> : IBucket<TIdentity>
         where TIdentity : IIdentity
     {
         #region Public properties
 
-        public long MaxSize { get; }
-        public TIdentity OwnerIdentity { get; }
-        public TIdentity PoolIdentity { get; }
-        public TIdentity SelfIdentity { get; }
+        public long MaxSize => _getMaxSizePropertyMethod.InvokeAndWait(Nothing.Instance);
+        public TIdentity OwnerIdentity => _getOwnerIdentityPropertyMethod.InvokeAndWait(Nothing.Instance);
+        public TIdentity PoolIdentity => _getPoolIdentityPropertyMethod.InvokeAndWait(Nothing.Instance);
+        public TIdentity SelfIdentity => _getSelfIdentityPropertyMethod.InvokeAndWait(Nothing.Instance);
 
         #endregion
 
@@ -27,11 +30,18 @@
         private readonly IMethod<Manifest, Hash[]> _getHashesMethod;
         private readonly IMethod<Nothing, Manifest[]> _getManifestsMethod;
         private readonly IMethod<Tuple<Manifest, Hash[]>, Slice[]> _getSlicesMethod;
+        private readonly IMethod<Nothing, long> _getMaxSizePropertyMethod;
+        private readonly IMethod<Nothing, TIdentity> _getOwnerIdentityPropertyMethod;
+        private readonly IMethod<Nothing, TIdentity> _getPoolIdentityPropertyMethod;
+        private readonly IMethod<Nothing, TIdentity> _getSelfIdentityPropertyMethod;
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Creates a new <see cref="RemoteBucket{TIdentity}"/>, which implements the remote side of a networked <see cref="IBucket{TIdentity}"/>
+        /// </summary>
         public RemoteBucket(
             IProtocol protocol,
             IConverter<Nothing, byte[]> nothingToBytesConverter,
@@ -40,13 +50,18 @@
             IConverter<byte[], Hash[]> bytesToHashArrayConverter,
             IConverter<byte[], Manifest[]> bytesToManifestArrayConverter,
             IConverter<Tuple<Manifest, Hash[]>, byte[]> manifestAndHashArrayTupleToBytesConverter,
-            IConverter<byte[], Slice[]> bytesToSliceArrayConverter
+            IConverter<byte[], Slice[]> bytesToSliceArrayConverter,
+            IConverter<byte[], TIdentity> bytesToTIdentityConverter
             )
         {
             _getCurrentSizeMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.GetCurrentSize), nothingToBytesConverter, bytesToLongConverter, () => { });
             _getHashesMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.GetHashes), manifestToBytesConverter, bytesToHashArrayConverter, () => { });
             _getManifestsMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.GetManifests), nothingToBytesConverter, bytesToManifestArrayConverter, () => { });
+            _getMaxSizePropertyMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.MaxSize), nothingToBytesConverter, bytesToLongConverter, () => { });
+            _getOwnerIdentityPropertyMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.OwnerIdentity), nothingToBytesConverter, bytesToTIdentityConverter, () => { });
+            _getPoolIdentityPropertyMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.PoolIdentity), nothingToBytesConverter, bytesToTIdentityConverter, () => { });
             _getSlicesMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.GetSlices), manifestAndHashArrayTupleToBytesConverter, bytesToSliceArrayConverter, () => { });
+            _getSelfIdentityPropertyMethod = ProtocolMethod.Create(protocol, nameof(IBucket<TIdentity>.SelfIdentity), nothingToBytesConverter, bytesToTIdentityConverter, () => { });
         }
 
         #endregion
@@ -54,11 +69,8 @@
         #region Public methods
 
         public long GetCurrentSize() => _getCurrentSizeMethod.InvokeAndWait(Nothing.Instance);
-
         public IEnumerable<Hash> GetHashes(Manifest forManifest) => _getHashesMethod.InvokeAndWait(forManifest);
-
         public IEnumerable<Manifest> GetManifests() => _getManifestsMethod.InvokeAndWait(Nothing.Instance);
-
         public IEnumerable<Slice> GetSlices(Manifest forManifest, Hash[] hashes) => _getSlicesMethod.InvokeAndWait(Tuple.Create(forManifest, hashes));
 
         #endregion
