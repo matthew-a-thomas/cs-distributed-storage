2df4dca9cf1ec537705d5b4fcb9e19a8ae522be8
Matthew Thomas <me@matthewathomas.com>
2017-07-13 18:01:51 -0400

    Added token factory to create new tokens signed by the server's secret

---
+++ AspNet/Models/TokenFactory.cs
@@ -0,0 +1,36 @@
+﻿namespace AspNet.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using Authorization;
+
+    public class TokenFactory
+    {
+        private readonly SecretRepository _secretRepository;
+
+        public TokenFactory(SecretRepository secretRepository)
+        {
+            _secretRepository = secretRepository;
+        }
+
+        public Token GenerateNewToken()
+        {
+            var secret = _secretRepository.GetCachedSecret();
+            if (secret == null)
+                return null;
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var idBytes = new byte[8];
+                rng.GetBytes(idBytes);
+                var id = Convert.ToBase64String(idBytes);
+                var claims = new Dictionary<string, string>
+                {
+                    { "id", id}
+                };
+                var token = claims.ToToken(secret);
+                return token;
+            }
+        }
+    }
+}
