2c9d239d3420366978eeaf7ddd0a8bedefc9e8aa
Matthew Thomas <me@matthewathomas.com>
2017-07-13 18:00:44 -0400

    Added caching classes

---
+++ DistributedStorage/Storage/ICache.cs
@@ -0,0 +1,13 @@
+﻿namespace DistributedStorage.Storage
+{
+    /// <summary>
+    /// Something which knows how to cache values
+    /// </summary>
+    public interface ICache<out T>
+    {
+        /// <summary>
+        /// Retrieve the cached value if possible, or a new value
+        /// </summary>
+        T GetValue();
+    }
+}
---
+++ DistributedStorage/Storage/WeakCache.cs
@@ -0,0 +1,31 @@
+﻿namespace DistributedStorage.Storage
+{
+    using System;
+
+    /// <summary>
+    /// Something that uses a <see cref="WeakReference{T}"/> to maintain a cache
+    /// </summary>
+    public sealed class WeakCache<T> : ICache<T> where T : class
+    {
+        private readonly WeakReference<T> _cache = new WeakReference<T>(null);
+        private readonly Func<T> _generateValue;
+
+        public WeakCache(Func<T> generateValue)
+        {
+            _generateValue = generateValue;
+        }
+
+        public T GetValue()
+        {
+            // Try getting the value from the cache
+            if (_cache.TryGetTarget(out var value))
+                return value;
+
+            // Cache miss: generate a new one
+            value = _generateValue();
+            _cache.SetTarget(value);
+
+            return value;
+        }
+    }
+}
