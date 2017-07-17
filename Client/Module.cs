dd776b03269016ea572a787c1732f738534198ba
Matthew Thomas <me@matthewathomas.com>
2017-07-17 15:07:58 -0400

    Added Autofac

---
+++ Client/Module.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Client
+{
+    internal sealed class Module : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Program>().SingleInstance();
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -2,13 +2,26 @@
 
 namespace Client
 {
+    using Autofac;
+
     // ReSharper disable once UnusedMember.Global
     internal class Program
     {
         // ReSharper disable once UnusedMember.Local
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<Module>();
+            using (var container = builder.Build())
+            {
+                var program = container.Resolve<Program>();
+                program.Run();
+            }
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello world");
         }
     }
 }
