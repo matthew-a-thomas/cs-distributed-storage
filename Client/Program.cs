633c9810817382d817eecc89480f91efac906b96
Matthew Thomas <me@matthewathomas.com>
2017-07-17 15:26:18 -0400

    WIP: stubbed out kind of what the program should do

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,14 +2,25 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using DistributedStorage.Storage.Containers;
 
     internal partial class Program
     {
+        #region Private fields
+
+        private readonly IAddableContainer<string, Server> _serverContainer;
+
+        #endregion
+
         #region Constructor
 
-        public Program()
+        public Program(
+            IAddableContainer<string, Server> serverContainer
+            )
         {
-
+            _serverContainer = serverContainer;
         }
 
         #endregion
@@ -29,7 +40,41 @@
 
         #region Private methods
 
-        private void ManageOwnedServers() => throw new NotImplementedException();
+        private void AddAServer()
+        {
+            IPAddress ipAddress;
+            int port;
+            while (!IPAddress.TryParse("IP address?".Ask(), out ipAddress)) { }
+            while (!int.TryParse("Port?".Ask(), out port)) { }
+            var endpoint = new IPEndPoint(ipAddress, port);
+            var server = new Server(endpoint);
+            if (!_serverContainer.TryAdd(server.ToString(), server))
+                "Failed to add this server".Say();
+        }
+
+        private void DeleteServer(string serverName) => _serverContainer.TryRemove(serverName);
+
+        private void ListServers()
+        {
+            "Pick one".Choose(_serverContainer.GetKeysAndValues().ToDictionary(kvp => kvp.Key, kvp => new Action(() =>
+            {
+                var serverName = kvp.Key;
+                var server = kvp.Value;
+                "Do what with this server?".Choose(new Dictionary<string, Action>
+                {
+                    { "Delete it", () => DeleteServer(serverName) }
+                });
+            })));
+        }
+
+        private void ManageOwnedServers()
+        {
+            "Do what with servers?".Choose(new Dictionary<string, Action>
+            {
+                { "List servers", ListServers },
+                { "Add a server", AddAServer }
+            });
+        }
 
         private void UploadFile() => throw new NotImplementedException();
 
---
+++ Client/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace Client
+{
+    using System.Net;
+
+    public sealed class Server
+    {
+        public Server(IPEndPoint endpoint)
+        {
+            Endpoint = endpoint;
+        }
+
+        public IPEndPoint Endpoint { get; }
+
+        public override string ToString() => Endpoint.ToString();
+    }
+}
