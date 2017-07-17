78f3f65d3e72f8c589904d6c222e45387835cfe2
Matthew Thomas <me@matthewathomas.com>
2017-07-17 15:10:29 -0400

    Added string extensions back (pulled out of an old commit)

---
+++ Client/StringExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal static class StringExtensions
+    {
+        public static string Ask(this string question)
+        {
+            question.Say();
+            return Console.ReadLine();
+        }
+
+        public static void Choose(this string prompt, Dictionary<string, Action> choices)
+        {
+            while (true)
+            {
+                prompt.Say();
+                var numberToChoice = choices.Select((kvp, i) => new { Choice = kvp.Key, Number = i }).ToDictionary(x => x.Number, x => x.Choice);
+                int number;
+                {
+                    var i = 0;
+                    foreach (var choice in choices.Keys)
+                    {
+                        ++i;
+                        $"\t{i} - {choice}".Say();
+                    }
+                    var response = $"1-{i}?".Ask();
+                    int.TryParse(response, out number);
+                    --number;
+                }
+
+                if (numberToChoice.ContainsKey(number))
+                {
+                    var choice = numberToChoice[number];
+                    var action = choices[choice];
+                    action();
+                    return;
+                }
+
+                "Invalid choice".Say();
+            }
+        }
+
+        public static void Say(this string message) => Console.WriteLine(message);
+
+        public static void Wait(this string message)
+        {
+            message.Say();
+            Console.ReadKey(true);
+        }
+    }
+}
