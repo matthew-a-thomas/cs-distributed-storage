82367ec1b8f8699dc62594f1fb50f274119af4ce
Matthew Thomas <me@matthewathomas.com>
2017-07-17 15:04:21 -0400

    Started client console app

---
+++ Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Client
+{
+    // ReSharper disable once UnusedMember.Global
+    internal class Program
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
