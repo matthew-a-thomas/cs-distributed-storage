c1413d98b05d2099e0f16c6b5f2fb2c82927ddc4
Matthew Thomas <me@matthewathomas.com>
2017-07-19 10:30:20 -0400

    WIP: fleshing out a remote method call

--- Client/Remote/Controllers/RemoteManifestsController.cs
+++ Client/Remote/Controllers/RemoteManifestsController.cs
@@ -1,6 +1,9 @@
 ﻿namespace Client.Remote.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
     using DistributedStorage.Authentication;
     using DistributedStorage.Encoding;
     using DistributedStorage.Networking.Controllers;
@@ -14,20 +17,28 @@
             _credential = credential;
         }
 
-        public IReadOnlyList<string> GetManifestIds() => throw new System.NotImplementedException();
+        public IReadOnlyList<string> GetManifestIds()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Matt's distributed storage client");
 
-        public bool TryAddNewManifest(Manifest manifest) => throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
 
-        public bool TryDeleteManifest(string manifestId) => throw new System.NotImplementedException();
+        public bool TryAddNewManifest(Manifest manifest) => throw new NotImplementedException();
 
-        public bool TryGetManifest(string manifestId, out Manifest manifest) => throw new System.NotImplementedException();
+        public bool TryDeleteManifest(string manifestId) => throw new NotImplementedException();
 
-        public bool TryAddNewSlice(string manifestId, Slice slice) => throw new System.NotImplementedException();
+        public bool TryGetManifest(string manifestId, out Manifest manifest) => throw new NotImplementedException();
 
-        public bool TryGetSliceIds(string manifestId, out IReadOnlyList<string> sliceIds) => throw new System.NotImplementedException();
+        public bool TryAddNewSlice(string manifestId, Slice slice) => throw new NotImplementedException();
 
-        public bool TryDeleteSlice(string manifestId, string sliceId) => throw new System.NotImplementedException();
+        public bool TryGetSliceIds(string manifestId, out IReadOnlyList<string> sliceIds) => throw new NotImplementedException();
 
-        public bool TryGetSlice(string manifestId, string sliceId, out Slice slice) => throw new System.NotImplementedException();
+        public bool TryDeleteSlice(string manifestId, string sliceId) => throw new NotImplementedException();
+
+        public bool TryGetSlice(string manifestId, string sliceId, out Slice slice) => throw new NotImplementedException();
     }
 }
