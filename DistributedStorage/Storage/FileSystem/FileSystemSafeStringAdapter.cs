654818dae0429c94e9603c71d0dfeecef0fca8e3
Matt Thomas <me@matthewathomas.com>
2017-07-22 16:22:00 -0400

    Added class which can convert between strings that are unsafe as file/directory names and their safe equivalents

---
+++ DistributedStorage/Storage/FileSystem/FileSystemSafeStringAdapter.cs
@@ -0,0 +1,29 @@
+﻿namespace DistributedStorage.Storage.FileSystem
+{
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public sealed class FileSystemSafeStringAdapter
+    {
+        public const char EscapeChar = 'Z';
+        private static readonly Regex EscapeRegex = new Regex($"{EscapeChar}([0-9]+){EscapeChar}");
+        private static readonly char[] UnsafeCharacters = Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).Distinct().ToArray();
+        
+        public static string MakeRaw(string safe)
+        {
+            var raw = EscapeRegex.Replace(safe, match =>
+            {
+                var integer = int.Parse(match.Groups[1].Value);
+                return ((char)integer).ToString();
+            });
+            return raw;
+        }
+
+        public static string MakeSafe(string raw)
+        {
+            var safe = raw.Replace(EscapeChar.ToString(), $"{EscapeChar}{(int) EscapeChar}{EscapeChar}");
+            return UnsafeCharacters.Aggregate(safe, (current, unsafeCharacter) => current.Replace(unsafeCharacter.ToString(), $"{EscapeChar}{(int) unsafeCharacter}{EscapeChar}"));
+        }
+    }
+}
---
+++ DistributedStorageTests/Storage/FileSystem/FileSystemSafeStringAdapterClass.cs
@@ -0,0 +1,68 @@
+﻿namespace DistributedStorageTests.Storage.FileSystem
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using DistributedStorage.Storage.FileSystem;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FileSystemSafeStringAdapterClass
+    {
+        [TestMethod]
+        public void ConvertsFromRawToSafeBackToRawForLotsOfRandomStrings()
+        {
+            var random = new Random(0);
+            for (var i = 0; i < 10000; ++i)
+            {
+                var randomString = GenerateRandomString(random);
+                
+                var safe = FileSystemSafeStringAdapter.MakeSafe(randomString);
+                var raw = FileSystemSafeStringAdapter.MakeRaw(safe);
+
+                Assert.AreEqual(randomString, raw);
+            }
+        }
+
+        [TestMethod]
+        public void MakesSpecificStringsSafe()
+        {
+            var badCharacters = Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).ToArray();
+            
+            foreach (var testString in new[]
+            {
+                @"/../../..",
+                @"\..\..\.."
+            })
+            {
+                var goodString = FileSystemSafeStringAdapter.MakeSafe(testString);
+                foreach (var badCharacter in badCharacters)
+                {
+                    Assert.IsFalse(goodString.Contains(badCharacter), $"The \"safe\" string \"{goodString}\" contains the bad character \"{badCharacter}\"");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ProducesExpectedRawStringFromSafeStringWithInterleavedEscapeCharacters()
+        {
+            var safeString = $@"{FileSystemSafeStringAdapter.EscapeChar}32{FileSystemSafeStringAdapter.EscapeChar}32{FileSystemSafeStringAdapter.EscapeChar}32{FileSystemSafeStringAdapter.EscapeChar}";
+            var rawString = FileSystemSafeStringAdapter.MakeRaw(safeString);
+            Assert.AreEqual(@" 32 ", rawString);
+        }
+        
+        private static string GenerateRandomString(Random random)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var randomNumber in Enumerable.Repeat(0, 100).Select(_ => random.Next()))
+            {
+                if (randomNumber == 0)
+                    continue;
+                stringBuilder.Append((char) randomNumber);
+            }
+            var randomString = stringBuilder.ToString();
+            return randomString;
+        }
+    }
+}
