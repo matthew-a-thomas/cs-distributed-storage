6d63371df84643d98663d977a6f6becaa6ba6485
Matt Thomas <me@matthewathomas.com>
2017-07-22 22:58:43 -0400

    Fixed registrations so that credentials for owned servers are now persisted to disk

--- Client/Modules/Storage.cs
+++ Client/Modules/Storage.cs
@@ -1,14 +1,41 @@
 ﻿namespace Client.Modules
 {
+    using System.IO;
+    using System.Reflection;
     using Autofac;
     using Client.Storage;
+    using DistributedStorage.Storage.Containers;
+    using DistributedStorage.Storage.FileSystem;
+    using Module = Autofac.Module;
 
     public sealed class Storage : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
+            // Register the "working directory" directory as an IDirectory
+            builder.Register(c =>
+                {
+                    const string workingDirectoryName = "working directory";
+                    var assemblyFileInfo = new FileInfo(Assembly.GetEntryAssembly().Location);
+                    var assemblyFolder = assemblyFileInfo.Directory;
+                    var workingDirectory = assemblyFolder.CreateSubdirectory(workingDirectoryName);
+                    var adapter = new DirectoryInfoToDirectoryAdapter(workingDirectory);
+                    return adapter;
+                })
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
             // Use on-disk storage of credentials
-            builder.RegisterType<UriToCredentialFileBasedContainer>().AsImplementedInterfaces().SingleInstance();
+            builder.Register(c =>
+                {
+                    const string ownedServerCredentialsDirectoryName = "owned servers";
+                    var workingDirectory = c.Resolve<IDirectory>();
+                    var ownedServerCredentialsDirectory = workingDirectory.Directories.GetOrCreate(ownedServerCredentialsDirectoryName);
+                    var container = new UriToCredentialFileBasedContainer(ownedServerCredentialsDirectory);
+                    return container;
+                })
+                .AsImplementedInterfaces()
+                .SingleInstance();
         }
     }
 }
